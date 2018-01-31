334775c970b7978342875725003b36ecc7cdcaec
harry margalotti <imharrymargalotti@gmail.com>
2018-01-31 01:09:02 -0500

    tests for 2 numbers

--- src/HWFunctionsimpl.java
+++ src/HWFunctionsimpl.java
@@ -4,15 +4,15 @@
 public class HWFunctionsimpl implements HWFunctions {
     public int Add(String numbers){
         int sum=0;
-        //------------0 numbers in string------------------
+        //------------0 numbers in string----------------------------
         if (numbers.equals("")){
             return sum;
         }
-        //------------1 number in string------------------
+        //------------1 number in string-----------------------------
         else if (numbers.indexOf(',')==-1){
             sum = Integer.parseInt(numbers);
         }
-
         return sum;
     }
+
 }
--- test/HWFunctionsimplTest.java
+++ test/HWFunctionsimplTest.java
@@ -40,7 +40,41 @@ class HWFunctionsimplTest {
         int resultE = myFunctions.Add(test2E);
         assertEquals(-10000,resultE);
 
-        //--------------PARTITION3-----------------------------------------------
+        //--------------PARTITION3 2 NUMBERS--------------------------------------
+        //test a: 2 small positive numbers
+        String test3A = "5,5";
+        int result3A = myFunctions.Add(test2A);
+        assertEquals(10,result3A);
+
+        //test b: boundary value of 0 and 1
+        String test3B = "0,1";
+        int result3B = myFunctions.Add(test3B);
+        assertEquals(1,result3B);
+
+        //test c: boundary value of 0 and -1
+        String test3C = "0,-1";
+        int result3C = myFunctions.Add(test3C);
+        assertEquals(1,result3C);
+
+        //test d: small negative numbers
+        String test3D = "-5,-5";
+        int result3D = myFunctions.Add(test3D);
+        assertEquals(-10,result3D);
+
+        //test e: large positive numbers
+        String test3E = "10000,10000";
+        int result3E = myFunctions.Add(test2E);
+        assertEquals(20000,result3E);
+
+        //test f: large negative numbers
+        String test3F = "-10000,-10000";
+        int result3F = myFunctions.Add(test3F);
+        assertEquals(-20000,result3F);
+
+        //test g: positive and negative number
+        String test3G = "100,-50";
+        int result3G = myFunctions.Add(test3G);
+        assertEquals(50,result3G);
     }
 
 }
