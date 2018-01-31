5fd5e41be44cb61d550abfb02ea589bd625fdad5
harry margalotti <imharrymargalotti@gmail.com>
2018-01-31 00:34:32 -0500

    tests for 1 number: tested 5 cases

--- test/HWFunctionsimplTest.java
+++ test/HWFunctionsimplTest.java
@@ -8,16 +8,39 @@ import static org.junit.jupiter.api.Assertions.*;
 class HWFunctionsimplTest {
     @Test
     void add() {
-        //PARTITON1 NO NUMBERS
+        //--------------PARTITON1 NO NUMBERS-------------------------------------
         String test = "";
         HWFunctions myFunctions = new HWFunctionsimpl();
         int result = myFunctions.Add(test);
         assertEquals(0,result);
 
-        //PARTITION2 1 NUMBER
-        String test2 = "5";
+        //--------------PARTITION2 1 NUMBER--------------------------------------
+        //test a: small positive number
+        String test2A = "5";
+        int resultA = myFunctions.Add(test2A);
+        assertEquals(5,resultA);
 
-        //PARTITION 3
+        //test b: boundary value of 0
+        String test2B = "0";
+        int resultB = myFunctions.Add(test2B);
+        assertEquals(0,resultB);
+
+        //test c: small negative number
+        String test2C = "-5";
+        int resultC = myFunctions.Add(test2C);
+        assertEquals(-5,resultC);
+
+        //test d: large positive number
+        String test2D = "10000";
+        int resultD = myFunctions.Add(test2D);
+        assertEquals(10000,resultD);
+
+        //test e: large negative number
+        String test2E = "-10000";
+        int resultE = myFunctions.Add(test2E);
+        assertEquals(-10000,resultE);
+
+        //--------------PARTITION3-----------------------------------------------
     }
 
 }
